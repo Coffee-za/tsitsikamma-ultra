e76ef3cbad68e281d15f20db028723b821feed00
Hezzie Ponelat <hponelat@gmail.com>
2025-08-20 17:09:57 +0200

    Fixed SEO issues, changed OpenGraph image

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 // IMPORTANT: Set your production site URL so canonical links and sitemaps use absolute URLs
 export default defineConfig({
 	// Replace with your live domain, including protocol, no trailing slash
-	site: 'https://www.tsitsikammaultra.co.za',
+	site: 'https://tsitsikammaultra.co.za',
 	integrations: [
 		sitemap({
 			filter: (page) => !['/404', '/success'].includes(page),
